f1743bb5aa7ae84ddfda74fbffbef513364ee022
Vennela Varshini Anasoori <vennelavarshini07@gmail.com>
2025-12-14 21:14:36 +0530

    Refactor Vite config
    Refactor Vite config to conditionally set server port and add build output directory.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    open: false
-  }
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    server: {
+      // Only use port for dev server locally
+      ...(command === "serve" && {
+        port: 5173,
+        open: false,
+      }),
+    },
+    build: {
+      outDir: "dist", // Vercel needs this
+    },
+  };
 });
